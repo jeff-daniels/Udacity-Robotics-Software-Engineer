f0ae2e7a671bf1c00bb7a7d3edbd29422cc99989
Jeff <jeffrey.arthur.daniels@gmail.com>
2020-02-18 22:31:11 +0000

    initial commit

---
+++ Project 5: Home Service Robot/scripts/test_slam.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/Plano_IL.world x:=-8 " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+# or use slam_gmapping
+sleep 5
+
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch "
+
