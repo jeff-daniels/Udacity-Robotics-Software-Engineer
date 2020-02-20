64b559b3dcf87c379f0675c4f021639b118a7b7c
Jeff <jeffrey.arthur.daniels@gmail.com>
2020-02-20 20:02:49 +0000

    modify launch and script files to work with Plano_IL world

--- Project 5: Home Service Robot/scripts/test_navigation.sh
+++ Project 5: Home Service Robot/scripts/test_navigation.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm -e " export TURTLEBOT_GAZEBO_WORLD_FILE="$(cd ../ && pwd)/map/Plano_IL.world";
+roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
 
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch "  &
+xterm -e " export TURTLEBOT_GAZEBO_MAP_FILE="$(cd ../ && pwd)/map/map.yaml";
+roslaunch turtlebot_gazebo amcl_demo.launch "  &
 # or use slam_gmapping
 sleep 5
 
